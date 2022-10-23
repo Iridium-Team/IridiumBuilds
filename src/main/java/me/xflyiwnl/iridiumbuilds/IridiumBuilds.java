ace57e9019bb7a452860eb546d4906d326b135d4
xflyiwnl <erasylunerbekxd@gmail.com>
2022-10-23 16:30:34 +0600

    .

--- src/main/java/me/xflyiwnl/iridiumbuilds/IridiumBuilds.java
+++ src/main/java/me/xflyiwnl/iridiumbuilds/IridiumBuilds.java
@@ -1,8 +1,19 @@
 package me.xflyiwnl.iridiumbuilds;
 
+import com.palmergames.bukkit.towny.TownyCommandAddonAPI;
+import com.palmergames.bukkit.towny.object.AddonCommand;
+import me.xflyiwnl.iridiumbuilds.commands.BuildsCommand;
+import me.xflyiwnl.iridiumbuilds.configuration.BuildsConfiguration;
 import me.xflyiwnl.iridiumbuilds.objects.Build;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,10 +25,35 @@ public final class IridiumBuilds extends JavaPlugin {
         return instance;
     }
 
+    private PluginManager pluginManager;
+    private Plugin towny;
+
     @Override
     public void onEnable() {
+
         instance = this;
+
+        pluginManager = Bukkit.getPluginManager();
+        towny = pluginManager.getPlugin("Towny");
+        if (towny == null) {
+            Bukkit.getLogger().info(ChatColor.RED + "IridiumIdeology | Towny не обнаружено");
+            Bukkit.getLogger().info(ChatColor.RED + "IridiumIdeology | IridiumIdeology был выключен");
+
+            pluginManager.disablePlugin(this);
+        } else {
+            Bukkit.getLogger().info(ChatColor.GREEN + "IridiumIdeology | Подключение к Towny");
+            Bukkit.getLogger().info(ChatColor.GREEN + "IridiumIdeology | IridiumIdeology был включен");
+        }
+
+        BuildsConfiguration.generateDatabaseYaml();
+
+        setupBuilds();
+        setupCommands();
     }
+
+    /*
+            builds
+     */
 
     public Set<Build> builds = new HashSet<Build>();
 
@@ -41,6 +77,40 @@ public final class IridiumBuilds extends JavaPlugin {
             }
         }
         return null;
+    }
+
+    public void setupBuilds() {
+
+        builds.add(new Build("Резиденция"));
+        builds.add(new Build("Рынок"));
+        builds.add(new Build("Крепость"));
+        builds.add(new Build("Порт"));
+        builds.add(new Build("Верфь"));
+        builds.add(new Build("Университет"));
+        builds.add(new Build("Лаборатория"));
+        builds.add(new Build("Аэропорт"));
+        builds.add(new Build("Оружейный завод"));
+        builds.add(new Build("Авиационный завод"));
+        builds.add(new Build("Металлообрабатывающий завод"));
+        builds.add(new Build("Склад"));
+
+    }
+
+    /*
+            commands
+     */
+
+    public HashMap<String, CommandExecutor> commands = new HashMap<String, CommandExecutor>();
+
+    public HashMap<String, CommandExecutor> getCommands() {
+        return commands;
+    }
+
+    public void setupCommands() {
+
+        AddonCommand builds = new AddonCommand(TownyCommandAddonAPI.CommandType.TOWN, "builds", new BuildsCommand());
+        TownyCommandAddonAPI.addSubCommand(builds);
+
     }
 
 }
--- src/main/java/me/xflyiwnl/iridiumbuilds/commands/BuildsCommand.java
+++ src/main/java/me/xflyiwnl/iridiumbuilds/commands/BuildsCommand.java
@@ -1,7 +1,17 @@
 package me.xflyiwnl.iridiumbuilds.commands;
 
-public class BuildsCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class BuildsCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
 
 
+        return true;
+    }
 }
